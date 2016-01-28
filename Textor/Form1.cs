0249db7ad0103d72d48a37c18eb4c6e3f1fd4d04
Ihor01 <Ih01@i.ua>
2016-01-28 11:31:34 +0200

    CloseAction

--- Textor/Form1.cs
+++ Textor/Form1.cs
@@ -16,5 +16,10 @@ namespace Textor
         {
             InitializeComponent();
         }
+
+        private void tsmiExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
